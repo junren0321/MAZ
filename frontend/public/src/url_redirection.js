c575dc5e7944acc9d83b2e5ecee58bb9ed0bac3f
junren0321 <jrphoa0321@gmail.com>
2024-05-20 06:11:44 +0800

    url redirection register/login page

---
+++ frontend/public/src/url_redirection.js
@@ -0,0 +1,18 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    const signupButton = document.getElementById('register-2');
+
+    signupButton.addEventListener('click', function() {
+        // Navigate to the signup page
+        window.location.href = './login.html';
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const signupButton = document.getElementById('register-1');
+
+    signupButton.addEventListener('click', function() {
+        // Navigate to the signup page
+        window.location.href = './register.html';
+    });
+});
