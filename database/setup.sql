9f5656ae99e43b6ad8ced1fd93934f4d69b181b2
junren0321 <jrphoa0321@gmail.com>
2024-05-20 20:40:14 +0800

    database update

--- database/setup.sql
+++ database/setup.sql
@@ -3,10 +3,14 @@ CREATE DATABASE IF NOT EXISTS MAZ;
 USE MAZ;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 DROP TABLE IF EXISTS books;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
+=======
+CREATE TABLE IF NOT EXISTS users (
+>>>>>>> 22dcb29 (database update)
 =======
 CREATE TABLE IF NOT EXISTS users (
 >>>>>>> 22dcb29 (database update)
@@ -19,6 +23,7 @@ CREATE TABLE IF NOT EXISTS users (
     security_answer VARCHAR(255) NOT NULL
 );
 
+<<<<<<< HEAD
 CREATE TABLE books (
     id INT AUTO_INCREMENT PRIMARY KEY,
     filename VARCHAR(255) NOT NULL,
@@ -26,6 +31,15 @@ CREATE TABLE books (
     authors VARCHAR(1000),
     language VARCHAR(50),
     tags VARCHAR(1000),
+=======
+CREATE TABLE IF NOT EXISTS images (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    filename VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    author VARCHAR(255),
+    language VARCHAR(50),
+    tags TEXT,
+>>>>>>> 22dcb29 (database update)
     publisher VARCHAR(255),
     publish_date DATE,
     translated_by VARCHAR(255),
@@ -33,6 +47,10 @@ CREATE TABLE books (
     upload_date DATETIME DEFAULT CURRENT_TIMESTAMP,
     page_count INT,
     isbn VARCHAR(20),
+<<<<<<< HEAD
     description TEXT,
+=======
+    remark TEXT,
+>>>>>>> 22dcb29 (database update)
     FOREIGN KEY (uploaded_by) REFERENCES users(id)
 );
