adaa0cb0fd6586ac64ef25774eb69dad4b2d5e12
junren0321 <jrphoa0321@gmail.com>
2024-05-22 19:12:56 +0800

    Development (#4)
    * directories
    
    * directories structure
    
    * chibaai
    
    * first time merge and fucking exhausted
    
    * url redirection register/login page
    
    * prototype
    
    * after merging
    
    * 統一html file的Title
    
    * change 'redirect' name back to 'a'
    
    * Junren (#1)
    
    * update sign up to register
    
    * update url_redirection.js
    
    * split app.js into app.js and server.js
    
    * database update
    
    * update README
    
    * comment debug
    
    * revised the sequence of steps
    
    * Add reminder to update DB_PASSWORD in .env file
    
    * update README
    
    * Gyeoweeee (#2)
    
    * hsha
    
    * aaa
    
    * modified html/css and new img
    
    * Delete client/tmp.html
    
    * css modified
    
    * delete node_modules
    
    * hsha
    
    * update styles.css
    
    * add pic
    
    * update styles.css
    
    * Update styles.css
    
    * aaa
    
    * css modified
    
    ---------
    
    Co-authored-by: gyeozai <b11902091@ntu.edu.tw>
    Co-authored-by: gyeozai <113844198+gyeozai@users.noreply.github.com>
    
    * Gyeoweeee (#3)
    
    * hsha
    
    * aaa
    
    * modified html/css and new img
    
    * Delete client/tmp.html
    
    * css modified
    
    * delete node_modules
    
    * hsha
    
    * update styles.css
    
    * add pic
    
    * update styles.css
    
    * Update styles.css
    
    * aaa
    
    * css modified
    
    * junren teach me
    
    * aaa
    
    * css modified
    
    * hsha
    
    * aaa
    
    * css modified
    
    * junren teach me
    
    ---------
    
    Co-authored-by: gyeozai <b11902091@ntu.edu.tw>
    Co-authored-by: gyeozai <113844198+gyeozai@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Not_Tako <dicksontio02@gmail.com>
    Co-authored-by: gyeozai <b11902091@ntu.edu.tw>
    Co-authored-by: gyeozai <113844198+gyeozai@users.noreply.github.com>

---
+++ server/server.js
@@ -0,0 +1,9 @@
+// backend/server.js
+
+const app = require('./app');
+const PORT = process.env.PORT || 3000;
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
