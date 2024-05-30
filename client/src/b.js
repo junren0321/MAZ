08f1ce54c6dca785642074ce3811b40ee9e7f8a8
junren0321 <jrphoa0321@gmail.com>
2024-05-30 20:19:40 +0800

    merge

--- client/src/b.js
+++ client/src/b.js
@@ -17,6 +17,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
         const bookISBN = document.getElementById('book-isbn');
         const bookPageCount = document.getElementById('book-count');
         const bookDescription = document.getElementById('book-description');
+        const viewButton = document.getElementById('view-book');
+
+        const formattedDate = book.publishDate.split('T')[0];
 
         bookCover.src = book.url;
         bookName.textContent = book.name;
@@ -24,21 +27,31 @@ document.addEventListener('DOMContentLoaded', (event) => {
         bookLanguage.textContent = book.language;
         bookTags.textContent = book.tags.join(', ');
         bookPublisher.textContent = book.publisher;
+<<<<<<< HEAD
         // format the date
         const publishDate = new Date(book.publishDate);
         const formattedPublishDate = `${publishDate.getFullYear()}/${publishDate.getMonth() + 1}/${publishDate.getDate()}`;
         bookPublishDate.textContent = formattedPublishDate;
 
+=======
+        // bookPublishDate.textContent = book.publishDate;
+        bookPublishDate.textContent = formattedDate;
+>>>>>>> 8fff4a3695558171482b1d06786fe6a7f1f91f90
         bookTranslator.textContent = book.translatedBy;
         bookISBN.textContent = book.isbn;
         bookPageCount.textContent = book.pageCount;
         bookDescription.textContent = book.description;
 
-        // 点击封面图片设置 PDF URL 并跳转到 view.html
         bookCover.addEventListener('click', () => {
             localStorage.setItem('pdfUrl', book.url);
-            console.log('PDF URL set in localStorage:', book.url);  // 调试日志
-            window.location.href = 'view.html';
+            console.log('PDF URL set in localStorage:', book.url);
+            window.location.href = './pdfjs/web/viewer.html';
+        });
+        
+        viewButton.addEventListener('click', () => {
+            localStorage.setItem('pdfUrl', book.url);
+            console.log('PDF URL set in localStorage:', book.url);
+            window.location.href = './pdfjs/web/viewer.html';
         });
     } else {
         console.error('No book found with the given ID in localStorage');
