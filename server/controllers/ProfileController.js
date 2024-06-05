59a32084aff5b95e856435a481a0ae2893aea63e
Saiko0324 <dicksontio02@gmail.com>
2024-06-05 13:44:19 +0800

    all edit, delete done

---
+++ server/controllers/ProfileController.js
@@ -0,0 +1,229 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const db = require('../config/database');
+const nodemailer = require('nodemailer');
+
+// delete book
+exports.deletebook = async (req, res) => {
+    const { bookId } = req.body;
+
+    try {
+        if (bookId) {
+            const query = `
+                DELETE FROM books
+                WHERE id = ?;
+            `;
+
+            await db.query(query, [bookId]);
+            res.sendStatus(204); // No content response for successful deletion
+        } else {
+            res.status(400).json({ error: 'Invalid book id' }); // 400 for bad request
+        }
+    } catch (error) {
+        console.error('Error deleting book:', error);
+        res.status(500).json({ error: 'Database error during book delete.' });
+    }
+};
+
+// change email
+exports.changemail = async (req, res) => {
+    const { changemailpassword, oldemail, newemail } = req.body;
+    // console.log(req.body);
+
+    if (!changemailpassword || !oldemail || !newemail) {
+        console.error('Missing fields:', { changemailpassword, oldemail, newemail });
+        return res.status(400).json({ error: 'All fields are required' });
+    }
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.MAZ_EMAIL,
+            pass: process.env.MAZ_PASS
+        }
+    });
+    
+    try {
+        const [users] = await db.query('SELECT * FROM users WHERE email = ?', [oldemail]);
+        if (users.length > 0 && await bcrypt.compare(changemailpassword, users[0].password)) {
+            // console.log('Fetched users from database:', users);
+            const query = `
+            UPDATE users
+            SET email = ?
+            WHERE id = ?;
+            `;
+            try {
+                await db.query(query, [newemail, users[0].id]);
+                res.sendStatus(204); // No content response for successful update
+            } catch (error) {
+                console.error('Error editing email:', error);
+                res.status(500).json({ error: 'Error editing email' });
+            }
+
+            const mailOptions = {
+                from:process.env.MAZ_EMAIL,
+                to: newemail,
+                subject: 'New email for MAZ',
+                text: `Hi! We received your request for an email change, Pleased to meet you with your new identity!`
+            };
+
+            transporter.sendMail(mailOptions, (error, info) => {
+                if(error){
+                    console.error('Error sending email:', error);
+                    return res.status(500).json({ error: 'Error sending email.'});
+                } else {
+                    console.log('Email sent:', info.response);
+                    return res.json({ message: 'Please check your new email!'});
+                }
+            });
+
+        } else {
+            res.status(401).json({ error: 'Invalid email or password' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Database error during change mail.' });
+    }
+};
+
+// change password
+exports.changepassword = async (req, res) => {
+    const { currentpassword, newpassword, useremail } = req.body;
+    // console.log(req.body);
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.MAZ_EMAIL,
+            pass: process.env.MAZ_PASS
+        }
+    });
+    
+    try {
+        const [users] = await db.query('SELECT * FROM users WHERE email = ?', [useremail]);
+        if (users.length > 0 && await bcrypt.compare(currentpassword, users[0].password)) {
+            // console.log('Fetched users from database:', users);
+            const query = `
+            UPDATE users
+            SET password = ?
+            WHERE id = ?;
+            `;
+
+            const hashedPassword = await bcrypt.hash(newpassword, 10);
+
+            try {
+                await db.query(query, [hashedPassword, users[0].id]);
+                res.sendStatus(204); // No content response for successful update
+            } catch (error) {
+                console.error('Error changing password:', error);
+                res.status(500).json({ error: 'Error changing password' });
+            }
+
+            const mailOptions = {
+                from:process.env.MAZ_EMAIL,
+                to: useremail,
+                subject: 'New email for MAZ',
+                text: `Hi! We received your request for a password change. If you did not perform this action please contact us!`
+            };
+
+            transporter.sendMail(mailOptions, (error, info) => {
+                if(error){
+                    console.error('Error sending email:', error);
+                    return res.status(500).json({ error: 'Error sending email.'});
+                } else {
+                    console.log('Email sent:', info.response);
+                    return res.json({ message: 'Password change successful!'});
+                }
+            });
+        } else {
+            res.status(401).json({ error: 'Invalid password' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Database error during password change.' });
+    }
+};
+
+// change username
+exports.changeusername = async (req, res) => {
+    const { changenamepassword, newname, useremail } = req.body;
+    // console.log(req.body);
+
+    try {
+        const [users] = await db.query('SELECT * FROM users WHERE email = ?', [useremail]);
+        if (users.length > 0 && await bcrypt.compare(changenamepassword, users[0].password)) {
+            // console.log('Fetched users from database:', users);
+            const query = `
+            UPDATE users
+            SET username = ?
+            WHERE id = ?;
+            `;
+            try {
+                await db.query(query, [newname, users[0].id]);
+                res.sendStatus(204); // No content response for successful update
+            } catch (error) {
+                console.error('Error editing email:', error);
+                res.status(500).json({ error: 'Error editing email' });
+            }
+        } else {
+            res.status(401).json({ error: 'Invalid password' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Database error during change username.' });
+    }
+};
+
+// delete user
+exports.deleteuser = async (req, res) => {
+    const { deleteuserpassword, useremail } = req.body;
+
+    try {
+        const [users] = await db.query('SELECT * FROM users WHERE email = ?', [useremail]);
+        if (users.length > 0 && await bcrypt.compare(deleteuserpassword, users[0].password)) {
+            // console.log('Fetched users from database:', users);
+            const query = `
+            DELETE FROM users
+            WHERE id = ?;
+            `;
+
+            try {
+                await db.query(query, [users[0].id]);
+                res.sendStatus(204); // No content response for successful update
+            } catch (error) {
+                console.error('Error deleting user:', error);
+                res.status(500).json({ error: 'Error deleting user' });
+            }
+            
+            const transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: process.env.MAZ_EMAIL,
+                    pass: process.env.MAZ_PASS
+                }
+            });
+
+            const mailOptions = {
+                from:process.env.MAZ_EMAIL,
+                to: users[0].email,
+                subject: 'MAZ account deletion :(',
+                text: `Hi! We received your request for an account deletion, may we meet again!`
+            };
+
+            transporter.sendMail(mailOptions, (error, info) => {
+                if(error){
+                    console.error('Error sending email:', error);
+                    return res.status(500).json({ error: 'Error sending email.'});
+                } else {
+                    console.log('Email sent:', info.response);
+                    return res.json({ message: 'Please check your new email!'});
+                }
+            });
+        } else {
+            res.status(401).json({ error: 'Invalid password' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Database error during user delete.' });
+    }
+};
--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { register, login, forgot, changepass } = require('../controllers/UserController');
 const { uploadBook, getBooks, upload, getUserBooks, searchBooks} = require('../controllers/BookController');
 const authenticateToken = require('../util/authMiddleware');
+const { changemail, changepassword, changeusername, deleteuser, deletebook } = require('../controllers/ProfileController');
 
 router.post('/register', register);
 router.post('/login', login);
@@ -12,6 +13,11 @@ router.post('/forgot', forgot);
 router.put('/changepass', changepass);
 router.get('/userBooks', authenticateToken, getUserBooks);
 router.get('/searchBooks', searchBooks);
+router.put('/changemail', changemail);
+router.put('/changepassword', changepassword);
+router.put('/changeusername', changeusername);
+router.delete('/deleteuser', deleteuser);
+router.delete('/deletebook', deletebook);
 
 const { submitReview, fetchReviews, deleteReview, editReview } = require('../controllers/ReviewController');
 router.post('/reviews', submitReview);
